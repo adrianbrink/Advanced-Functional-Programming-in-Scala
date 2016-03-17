47d44d4ce6eadcd22dec5120a720e7b1720f8501
Adrian Brink <adrianbrink@users.noreply.github.com>
2016-03-17 19:05:15 +0100

    Almost finished 040

--- 040/Main.scala
+++ 040/Main.scala
@@ -1,5 +1,10 @@
-// Advanced Programming 2015
-// Andrzej Wąsowski, IT University of Copenhagen
+// FINAL
+//
+// Advanced Programming, Exercises by A. Wąsowski, IT University of Copenhagen
+//
+// AUTHOR1: Andrea Bitzili - abit@itu.dk
+// AUTHOR2: Adrian Brink - adbr@itu.dk
+// Group number: 43
 //
 // A script meant to be loaded into REPL (scala -i Main.scala)
 
@@ -7,11 +12,13 @@ import fpinscala.laziness._
 import fpinscala.laziness.Stream._
 
 // this is how we do simple interactive testing
-import exercises.laziness._
+// import exercises.laziness._
 val l1 :Stream[Int] = Empty
 val l2 :Stream[Int] = empty
 
-val l3 :Stream[Int]= cons(1, cons(2, cons (3, empty)))
+val l3 :Stream[Int]= cons(1, cons(2, cons (3, cons(4, empty))))
+
+val naturals: Stream[Int] = Stream.from(1)
 
 println (l1.headOption)
 println (l2.headOption)
--- 040/Stream.scala
+++ 040/Stream.scala
@@ -1,5 +1,10 @@
-// Advanced Programming 2015
-// Andrzej Wąsowski, IT University of Copenhagen
+// FINAL
+//
+// Advanced Programming, Exercises by A. Wąsowski, IT University of Copenhagen
+//
+// AUTHOR1: Andrea Bitzili - abit@itu.dk
+// AUTHOR2: Adrian Brink - adbr@itu.dk
+// Group number: 43
 //
 // meant to be compiled, for example: fsc Stream.scala
 
@@ -46,11 +51,69 @@ sealed trait Stream[+A] {
       // of ||)
     }
 
+    def toList(): List[A] = {
+      @annotation.tailrec
+      def go(s: Stream[A], acc: List[A]): List[A] = s match {
+        // Why does Cons(h(), acc) give me a type error but still
+        case Cons(h, t) => go(t(), h()::acc)
+        case _ => acc
+      }
+      go(this, List()).reverse
+    }
+
+    // What's the difference between Empty and empty?
+    def take(n: Int): Stream[A] = this match {
+      case Cons(x, xs) if n > 1 => Cons(() => x(), () => xs().take(n-1))
+      case Cons(x, _) if n == 1 => cons(x(), Empty)
+      case _ => Empty
+    }
+
+    @annotation.tailrec
+    final def drop(n: Int): Stream[A] = this match {
+      case Cons(_, xs) if n > 0 => xs().drop(n-1)
+      case _ => this
+    }
+
+    def takeWhile(f: A => Boolean): Stream[A] = this match {
+      case Cons(x, xs) if f(x()) => cons(x(), xs().takeWhile(f))
+      case _ => empty
+    }
+
+    def forAll(p: A => Boolean): Boolean = {
+      foldRight(true)((a, b) => p(a) && b)
+    }
+
+    def takeWhile1(f: A => Boolean): Stream[A] = {
+      foldRight(empty[A])((x, xs) =>
+        if (f(x)) cons(x, xs)
+        else empty)
+    }
+
+    def headOption1(): Option[A] = {
+      foldRight(None: Option[A])((x, _) => Some(x))
+    }
+
+    def map[B](p: A => B): Stream[B] = {
+      foldRight(empty[B])((x, xs) => cons(p(x), xs))
+    }
+
+    def filter(p: A => Boolean): Stream[B] = {
+      foldRight(empty[A])((x, xs) => 
+        if (p(x)) cons(x, xs)
+        else xs)
+    }
+
+    def append[B>:A](s: => Stream[B]): Stream[B] = {
+      foldRight(s)((x, xs) => cons(x, xs))
+    }
+
+    def flatMap[B](f: A => Stream[B]): Stream[B] = {
+      foldRight(empty[B])((x, xs) => f(x) append xs)
+    }
+
+
   //def find (p :A => Boolean) :Option[A] = this.filter (p).headOption
 }
-
-
-
 
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: ()=>A, t: ()=>Stream[A]) extends Stream[A]
@@ -72,6 +135,30 @@ object Stream {
     // Note 1: ":_*" tells Scala to treat a list as multiple params
     // Note 2: pattern matching with :: does not seem to work with Seq, so we
     //         use a generic function API of Seq
+  
+  def from(n: Int): Stream[Int] = {
+    cons(n, from(n+1))
+  }
+
+  // def to(n: Int): Stream[Int] = {
+  //   val l1: Stream[Int] = cons(1, empty)
+  //   if n > 0 {
+
+  //   }
+  //   cons(n, to(n-1)).reverse
+  //   def go(s: Stream[A], n: Int) {
+  //     s match {
+  //       case Cons()
+  //     }
+  //   }
+  // }
+  
+  val fibs = {
+    def fib(val1: Int, val2: Int): Stream[Int] = {
+      cons(val1, fib(val1, val1+val2))
+    }
+    fib(0, 1)
+  }
 }
 
 // vim:tw=0:cc=80:nowrap
